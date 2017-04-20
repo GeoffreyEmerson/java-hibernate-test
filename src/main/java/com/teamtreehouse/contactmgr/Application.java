13dd5de1f9467350eb49a3dcedf91b05a87cdf6c
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2017-04-19 19:31:07 -0700

    set up sessionfactory

--- src/main/java/com/teamtreehouse/contactmgr/Application.java
+++ src/main/java/com/teamtreehouse/contactmgr/Application.java
@@ -1,6 +1,26 @@
 package com.teamtreehouse.contactmgr;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
 public class Application {
+//    Hold a reusable reference to a SessionFactory (since we need only one)
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+
+        // Create a StandardServiceRegistry object
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        // calling configure() loads our configuration from the default location where we have
+        //  already set it up.
+        // calling build() makes the actual object, which gets stored in our registry var.
+
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        // no idea what all this is doing.
+    }
+
     public static void main(String[] args) {
 
     }
